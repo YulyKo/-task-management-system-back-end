0fe3a9c5e6e47b2004c467bbb984edd4ca4d556a
YulyKo <yuliakotenko1.0@gmail.com>
2021-06-16 01:01:51 +0300

    basic/created basic

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { AuthPage } from './pages/AuthPage';
 import { NotFound } from './pages/NotFound';
+import TodoPage from './pages/TodoPage';
 
 const Router = BrowserRouter;
 
@@ -14,6 +15,7 @@ ReactDOM.render(
       <Switch>
         <Route exact path="/" component={ HomePage } />
         <Route path="/auth" component={ AuthPage } />
+        <Route path="/todo-list" component={ TodoPage } />
         <Route component={ NotFound } />
       </Switch>
     </Router>
