dbfedea597a65e2c6171903b54ac1181fed5dd72
YulyKo <yuliakotenko1.0@gmail.com>
2021-06-19 22:42:06 +0300

    todo-task/refactored form

--- src/components/todo/TaskForm.js
+++ src/components/todo/TaskForm.js
@@ -15,52 +15,57 @@ export default class TaskForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      defaultTask: new Task(),
       titleError: '',
       descriptionError: '',
       priorityError: '',
-      newTaskFields: {},
+      task: {},
     };
   }
 
   componentDidMount() {
     if (this.props.task) {
       this.props.task.dueDate = this.formatDate(this.props.task.dueDate);
-    }
-    this.setState({ defaultTask: this.props.task ? this.props.task : new Task() });
+      this.setState({ task: this.props.task });
+    } else this.setState({ task: { dueDate: Date.now() } });
   }
 
-  handleSubmit(event) {
+  onSubmit(event) {
     event.preventDefault();
-    console.log(this.handleValidation(event));
+    console.log(this.state.task.dueDate);
+    // undefined because I dont set true value in this.handleValidation(event)
     if (this.handleValidation(event) === undefined) {
-      // undefined because I dont set true value in this.handleValidation(event)
-      this.checkDueDate();
-      // set task value
-      const task = this.compareTask();
-      // save data local
-      tasks.push(task);
-      console.log('http call here', tasks); // see local tasks
+      this.props.task ? this.update() : this.create();
       this.props.toggleHidden();
     }
   }
 
-  checkDueDate() {
-    let dueDateField = this.state.newTaskFields.dueDate;
-    dueDateField ? dueDateField =
-      Date.parse(dueDateField) :
-      dueDateField = Date.now();
+  create() {
+    // set task value
+    const task = this.compareTask();
+    // create data local
+    tasks.push(task);
+    // TODO add http calling here
+    console.log('http call here', tasks); // see local tasks
   }
 
-  handleChange(field, e){         
-    let fields = this.state.newTaskFields;
+  update() {
+    // set task value
+    const task = this.compareTask();
+    // update data local
+    tasks[task.id] = task;
+    // TODO add http calling here
+    console.log('http call here', tasks); // see local tasks
+  }
+
+  setFieldValue(field, e){         
+    let fields = this.state.task;
     fields[field] = e.target.value;        
     this.setState({fields});
   }
 
   checkExistRequired(fieldName) {
     const errorsArray = fieldName + 'Error';
-    if(!this.state.newTaskFields[fieldName]) {
+    if(!this.state.task[fieldName]) {
       this.setState({ [errorsArray]: 'Cannot be empty' });
       return false;
     } else this.setState({ [errorsArray]: '' });
@@ -76,42 +81,45 @@ export default class TaskForm extends Component {
   }
 
   formatDate(date) {
+    // generate date by seconds
     const d = new Date(date);
+    // get from d month, year and day
     let month = '' + (d.getMonth() + 1);
     let day = '' + d.getDate();
     let year = d.getFullYear();
 
+    // set zero to month and day
     if (month.length < 2) month = '0' + month;
     if (day.length < 2) day = '0' + day;
 
-    return [year, month, day].join('-');
+    return [year, month, day].join('-'); // => yyyy-mm-dd
   }
 
   compareTask = () => {
     const newTask = new Task();
-    const fieldsData = this.state.newTaskFields;
-    newTask.title = fieldsData.title ? fieldsData.title : this.state.defaultTask.title;
-    newTask.description = fieldsData.description ? fieldsData.description : this.state.defaultTask.description;
-    newTask.priority = fieldsData.priority ? fieldsData.priority : this.state.defaultTask.priority;
-    newTask.dueDate = fieldsData.dueDate ? fieldsData.dueDate : this.state.defaultTask.dueDate;
+    const fieldsData = this.state.task;
+    newTask.title = fieldsData.title;
+    newTask.description = fieldsData.description;
+    newTask.priority = fieldsData.priority;
+    newTask.dueDate = fieldsData.dueDate;
     return newTask;
   }
 
   render() {
-    return <form onSubmit={this.handleSubmit.bind(this)}>
+    return <form onSubmit={this.onSubmit.bind(this)}>
       <input
         type="text"
-        defaultValue={this.state.defaultTask.title}
-        onChange={this.handleChange.bind(this, 'title')} />
+        defaultValue={this.state.task.title}
+        onChange={this.setFieldValue.bind(this, 'title')} />
       <span className="error">{this.state.titleError}</span>
 
       <textarea
-        defaultValue={this.state.defaultTask.description}
-        onChange={this.handleChange.bind(this, 'description')}>
+        defaultValue={this.state.task.description}
+        onChange={this.setFieldValue.bind(this, 'description')}>
       </textarea>
       <span className="error">{this.state.descriptionError}</span>
 
-      <div onChange={this.handleChange.bind(this, 'priority')}>
+      <div onChange={this.setFieldValue.bind(this, 'priority')}>
         {PRIORITIES.map((data, index) =>
           <input key={index} type="radio" value={data} name="priority"/>
         )}
@@ -119,16 +127,17 @@ export default class TaskForm extends Component {
       <span className="error">{this.state.priorityError}</span>
 
       {
+        // I can't set default due date from this.componentDidMount()
         this.props.task ?
           <input
             type="date"
             defaultValue={this.formatDate(this.props.task.dueDate)}
-            onChange={this.handleChange.bind(this, 'dueDate')} />
+            onChange={this.setFieldValue.bind(this, 'dueDate')} />
           :
           <input
             type="date"
             defaultValue={this.formatDate(Date.now())}
-            onChange={this.handleChange.bind(this, 'dueDate')} />
+            onChange={this.setFieldValue.bind(this, 'dueDate')} />
       }
       <button type="submit">Add</button>
     </form>;
