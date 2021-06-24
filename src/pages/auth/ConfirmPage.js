85879fdf2cf7ad8cf2b8da0e8ac2cc9c0b9cb178
YulyKo <yuliakotenko1.0@gmail.com>
2021-06-24 17:19:05 +0300

    confirm-page/added auth.confirmUser()  with message

--- src/pages/auth/ConfirmPage.js
+++ src/pages/auth/ConfirmPage.js
@@ -1,3 +1,4 @@
+import { any } from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { authService } from '../../services';
@@ -9,24 +10,27 @@ export class ConfirmPage extends React.Component {
     super(props);
     this.state = {
       token: '',
+      message: any,
     };
   }
 
   componentDidMount() {
     const code = window.location.pathname.replace('/confirm/', '');
-    if (code) {
-      authService.confirmUser(code); 
-    }
+    authService.confirmUser(code)
+      .then((res) => {
+        this.setState({ message: res.message });
+      });
   }
 
   render() {
-    const { token } = this.state;
+    const { message } = this.state;
     return <main>
       <h1>For confirm click to button</h1>
+      { message }
       {
-        token === '' ?
-          <Link to="/login">go to login</Link> :
-          <Link to="/todo-list">go to tasks</Link>
+        message === 'Confirmed' ?
+          <Link to="/todo-list">Go to tasks</Link> :
+          <Link to="/login">Go to login</Link>
       }
     </main>;
   }
