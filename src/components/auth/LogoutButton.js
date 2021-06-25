75250027dfeb65071069a8bb469df2513d3c4865
YulyKo <yuliakotenko1.0@gmail.com>
2021-06-25 20:11:05 +0300

    http/fixed redirect after submit at logout form

--- src/components/auth/LogoutButton.js
+++ src/components/auth/LogoutButton.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import { ROOT_PAGE } from '../../navigation/paths.const';
 import { tokenService , authService} from '../../services/index';
-import { createBrowserHistory } from 'history';
 
 export class LogoutButton extends Component {
-  history = createBrowserHistory();
 
-  redirect() {
-    // redirect without reloading page, but after dont show root page
-    // console.log(this.history.location);
-    // this.history.push(ROOT_PAGE);
-    window.location.pathname = ROOT_PAGE;
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+    };
   }
 
   logout() {
     tokenService.cleanToken();
     authService.clearConfirmedStatus();
     authService.clearOwnerKey();
-    this.redirect();
+    this.setState({ clicked: true });
   }
 
   render() {
-    return <button onClick={this.logout.bind(this)}>Logout</button>;
+    const { clicked } = this.state;
+    return <>
+      <button onClick={this.logout.bind(this)}>Logout</button>
+      {
+        clicked ? <Redirect to={ROOT_PAGE} /> : ''
+      }
+    </>;
   }
 }
